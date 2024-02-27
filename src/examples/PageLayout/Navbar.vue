587d95a4f1a2b474abe4d22577c4d503fb08d5a5
unknown <saputranurahman24@gmail.com>
2024-02-27 11:13:40 +0700

    Donasi

---
+++ src/examples/PageLayout/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg top-0 z-index-3 position-absolute mt-4"
+    :class="isBlur ? isBlur : 'shadow-none my-2 navbar-transparent w-100'">
+    <div class="container ps-2 pe-0">
+  <router-link class="navbar-brand font-weight-bolder ms-0 ms-lg-3" :class="darkMode ? 'text-black' : 'text-white'" to="/">Khansia Donasi</router-link>
+  <button class="shadow-none navbar-toggler ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="mt-2 navbar-toggler-icon">
+      <span class="navbar-toggler-bar bar1"></span>
+      <span class="navbar-toggler-bar bar2"></span>
+    </span>
+  </button>
+  
+<ul class="navbar-nav d-lg-block d-none">
+  <li class="nav-item ms-auto">
+    <a href="https://www.creative-tim.com/product/vue-argon-dashboard" class="btn btn-sm mb-0 me-1" :class="isBtn">Dapatkan Aplikasi</a>
+  </li>
+</ul>
+
+</div>
+
+  </nav>
+  <!-- End Navbar -->
+</template>
+
+<script>
+import downArrWhite from "@/assets/img/down-arrow-white.svg";
+import downArrBlack from "@/assets/img/down-arrow-dark.svg";
+
+export default {
+  name: "navbar",
+  data() {
+    return {
+      downArrWhite,
+      downArrBlack
+    };
+  },
+  props: {
+    btnBackground: String,
+    isBlur: String,
+    isBtn: { type: String, default: "bg-gradient-light" },
+    darkMode: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    darkModes() {
+      return {
+        "text-dark": this.darkMode
+      };
+    }
+  }
+};
+</script>
---
+++ src/views/components/BillingCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="card-header pb-0 px-3">
+      <h6 class="mb-0">Billing Information</h6>
+    </div>
+    <div class="card-body pt-4 p-3">
+      <ul class="list-group">
+        <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+            <h6 class="mb-3 text-sm">Title Donasi</h6>
+            <span class="mb-2 text-xs">
+              Total Terkumpul:
+              <span class="text-dark font-weight-bold ms-sm-2">Rp. 50.000</span>
+            </span>
+            <span class="mb-2 text-xs">
+              Tanggal Di Buat :
+              <span class="text-dark ms-sm-2 font-weight-bold">09.10.2023</span>
+            </span>
+            <span class="text-xs">
+              Tanggal Selsai      :
+              <span class="text-dark ms-sm-2 font-weight-bold">09.11.2023</span>
+            </span>
+          </div>
+          <div class="ms-auto text-end">
+            <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+            <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+          </div>
+        </li>
+         
+        
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "billing-card",
+};
+</script>
